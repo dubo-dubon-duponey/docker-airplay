f07df321c3a80626e9f32593f2522eff3ebf0abd
dubo-dubon-duponey <dubodubonduponey+github@pm.me>
2021-09-03 10:44:26 -0700

    Attempt at preventing pulse from acquiring an exclusive lock on the device
    Signed-off-by: dubo-dubon-duponey <dubodubonduponey+github@pm.me>

--- context/runtime/boot/entrypoint.sh
+++ context/runtime/boot/entrypoint.sh
@@ -22,7 +22,7 @@ helpers::dbus(){
 if [ "${AIRPLAY_VERSION:-}" == 2 ]; then
   # https://superuser.com/questions/1157370/how-to-configure-pulseaudio-to-input-output-via-alsa
   mkdir -p /tmp/pulse-config/.config/pulse
-  printf "load-module module-alsa-sink device=$DEVICE\nload-module module-alsa-source device=$DEVICE\nload-module module-native-protocol-unix\n" > /tmp/pulse-config/.config/pulse/default.pa
+  printf "load-module module-suspend-on-idle\nload-module module-alsa-sink device=$DEVICE\nload-module module-alsa-source device=$DEVICE\nload-module module-native-protocol-unix\n" > /tmp/pulse-config/.config/pulse/default.pa
 
   # Pulse will create the socket in /tmp/pulse/native
   export XDG_RUNTIME_DIR=/tmp/pulse-socket
@@ -36,6 +36,8 @@ if [ "${AIRPLAY_VERSION:-}" == 2 ]; then
   cd /tmp
   export PULSE_SERVER="$XDG_RUNTIME_DIR"/pulse/native
   exec goplay2 -n "$MDNS_NAME" "$@"
+
+# XXX allow debugging with PULSE_SERVER=/tmp/pulse-socket/pulse/native HOME=/tmp/pulse-config pactl info
 fi
 
 args=(--port "$PORT" --output "$OUTPUT" --name "$MDNS_NAME" --use-stderr --mdns tinysvcmdns --configfile /config/shairport-sync/main.conf)
