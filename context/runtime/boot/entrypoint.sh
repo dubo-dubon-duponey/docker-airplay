e6f183d73194127f8f18921be6ff16b064c29718
dubo-dubon-duponey <dubodubonduponey+github@pm.me>
2021-10-15 16:51:32 -0700

    RC
    Signed-off-by: dubo-dubon-duponey <dubodubonduponey+github@pm.me>

--- context/runtime/boot/entrypoint.sh
+++ context/runtime/boot/entrypoint.sh
@@ -9,16 +9,41 @@ source "$root/helpers.sh"
 helpers::dir::writable "/tmp"
 
 # Traditional, shairport-sync airplay goes here
-if [ "${AIRPLAY_VERSION:-}" != 2 ]; then
+if [ "${_EXPERIMENTAL_AIRPLAY_VERSION:-}" != 2 ]; then
   args=(--port "$PORT" --output "$OUTPUT" --name "$MDNS_NAME" --use-stderr --mdns tinysvcmdns --configfile /config/shairport-sync/main.conf)
 
   [ "$(printf "%s" "$LOG_LEVEL" | tr '[:upper:]' '[:lower:]')" != "debug" ] || args+=(-vvv --statistics)
+  args+=("$@")
+  [ ! "$DEVICE" ] || [ "$OUTPUT" != "alsa" ] || args+=(-- -d "$DEVICE")
 
-  exec shairport-sync "${args[@]}" "$@"
+  exec shairport-sync "${args[@]}"
 fi
 
+# Options that are currently not implemented (can still be used by stuffing the command-line)
+#    -S, --stuffing=MODE set how to adjust current latency to match desired latency, where
+#                            "basic" inserts or deletes audio frames from packet frames with low processor overhead, and
+#                            "soxr" uses libsoxr to minimally resample packet frames -- moderate processor overhead.
+#                            "soxr" option only available if built with soxr support.
+#    -B, --on-start=PROGRAM  run PROGRAM when playback is about to begin.
+#    -E, --on-stop=PROGRAM   run PROGRAM when playback has ended.
+#                            For -B and -E options, specify the full path to the program, e.g. /usr/bin/logger.
+#                            Executable scripts work, but must have the appropriate shebang (#!/bin/sh) in the headline.
+#    --password=PASSWORD     require PASSWORD to connect. Default is not to require a password.
+#    --logOutputLevel        log the output level setting -- useful for setting maximum volume.
+#    -M, --metadata-enable   ask for metadata from the source and process it.
+#    --metadata-pipename=PIPE send metadata to PIPE, e.g. --metadata-pipename=/tmp/shairport-sync-metadata.
+#                            The default is /tmp/shairport-sync-metadata.
+#    -g, --get-coverart      send cover art through the metadata pipe.
+
+# Alsa backend
+#    -d output-device    set the output device, default is "default".
+#    -c mixer-control    set the mixer control name, default is to use no mixer.
+#    -m mixer-device     set the mixer device, default is the output device.
+#    -i mixer-index      set the mixer index, default is 0.
+
+
 ##############################################################################################
-# goplay / airplay2 goes here
+# goplay / airplay2 goes below
 ##############################################################################################
 # Right, this just does not work with iOS anymore - plus, sync sucks, plus, pulseaudio is a dumpster fire
 # So, use at your own peril
