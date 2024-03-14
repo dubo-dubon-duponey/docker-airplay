7d0560b8555f8d103cbe832051aad8f1e23d6947
dubo-dubon-duponey <dubodubonduponey+github@pm.me>
2024-03-13 21:28:23 -0700

    fix hosed BK and test scripts

--- hack/helpers/start-buildkit.sh
+++ hack/helpers/start-buildkit.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 set -o errexit -o errtrace -o functrace -o nounset -o pipefail
 
-export SUITE=bookworm
-export DATE=2024-02-20
-
-readonly IMAGE_BLDKT="${IMAGE_BLDKT:-docker.io/dubodubonduponey/buildkit:$SUITE-$DATE}"
+export TAG=latest
+readonly IMAGE_BLDKT="${IMAGE_BLDKT:-docker.io/dubodubonduponey/buildkit:$TAG}"
 
 setup::buildkit() {
   [ "$(docker container inspect -f '{{.State.Running}}' dbdbdp-buildkit 2>/dev/null)" == "true" ]  || {
@@ -20,6 +18,7 @@ setup::buildkit() {
       --privileged \
       "$IMAGE_BLDKT"
     docker exec --env QEMU_BINARY_PATH=/boot/bin/ dbdbdp-buildkit binfmt --install all
+    docker exec dbdbdp-buildkit mkdir /tmp/runtime
   }
 }
 
